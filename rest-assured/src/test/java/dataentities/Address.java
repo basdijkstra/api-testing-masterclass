ea401eb1b469a0f58f0a4ea19203368a0d31cfc3
Bas Dijkstra <bas@ontestautomation.com>
2020-04-29 12:54:49 +0200

    Use getters and setters in dataentities instead of Lombok to prevent issues on other machines

--- rest-assured/src/test/java/dataentities/Address.java
+++ rest-assured/src/test/java/dataentities/Address.java
@@ -1,8 +1,5 @@
 package dataentities;
 
-import lombok.Data;
-
-@Data
 public class Address {
 
     private String street;
@@ -18,6 +15,38 @@ public class Address {
         this.street = street;
         this.houseNumber = houseNumber;
         this.zipCode = zipCode;
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
         this.city = city;
     }
 }
