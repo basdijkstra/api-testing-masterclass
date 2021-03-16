39dfa0d9b5f01c441da8d587e404408fb9d43334
Bas Dijkstra <bas@ontestautomation.com>
2021-03-16 14:17:25 +0100

    Remove lombok from consumer implementation

--- consumer/src/main/java/consumer/ConsumerApp.java
+++ consumer/src/main/java/consumer/ConsumerApp.java
@@ -1,11 +1,9 @@
 package consumer;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class ConsumerApp {
 
     public static void main(String[] args) {
--- consumer/src/main/java/consumer/Location.java
+++ consumer/src/main/java/consumer/Location.java
@@ -1,15 +1,47 @@
 package consumer;
 
 import java.util.List;
-import lombok.Builder;
-import lombok.Data;
 
-@Data
-@Builder
 public class Location {
 
     private String zipCode;
     private String country;
     private String countryAbbreviation;
     private List<Place> places;
+
+    public Location() {
+
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountryAbbreviation() {
+        return countryAbbreviation;
+    }
+
+    public void setCountryAbbreviation(String countryAbbreviation) {
+        this.countryAbbreviation = countryAbbreviation;
+    }
+
+    public List<Place> getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(List<Place> places) {
+        this.places = places;
+    }
 }
